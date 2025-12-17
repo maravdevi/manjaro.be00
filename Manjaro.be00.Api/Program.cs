e79c50a306deac05c96b3fefa509a08f5c61b7d9
Frodo <marav.devpro@gmail.com>
2025-12-17 11:18:42 -0500

    1.main

--- Manjaro.be00.Api/Program.cs
+++ Manjaro.be00.Api/Program.cs
@@ -3,9 +3,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-// Registrar Swagger
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
